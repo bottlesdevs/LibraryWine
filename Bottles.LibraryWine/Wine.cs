100dae3793ffee5fe413d29ca854ee6d35a3799c
mirkobrombin <send@mirko.pm>
2021-07-17 16:01:38 +0200

    add DllOverrideTypes,RegeditKTypes,WindowsVersions

--- Bottles.LibraryWine/Wine.cs
+++ Bottles.LibraryWine/Wine.cs
@@ -10,8 +10,7 @@ namespace Bottles.LibraryWine
         private string WinePrefixPath { get; set; }
         private int VerboseLevel { get; set; }
 
-        // create enum for verbose levels (0, 1, 2, 3)
-        public enum VerboseLevels
+        private enum VerboseLevels
         {
             N_All = 0,
             N_WARN_Y_ALL = 1,
@@ -19,21 +18,91 @@ namespace Bottles.LibraryWine
             Y_ALL = 3
         }
 
-        
-        public Wine(string winePath, string winePrefixPath, int verboseLevel=0)
+        private enum DllOverrideTypes
+        {
+            BUILTIN = 0,
+            NATIVE = 1,
+            BUILTIN_NATIVE = 2,
+            NATIVE_BUILTIN = 3
+        }
+
+        private enum RegeditKTypes
+        {
+            REG_SZ = 0,
+            REG_DWORD = 1,
+            REG_MULTI_SZ = 2,
+            REG_BINARY = 3,
+            REG_EXPAND_SZ = 4,
+            REG_NONE = 5
+        }
+
+        public Dictionary<string, WindowsVersion> WindowsVersions = new Dictionary<string, WindowsVersion>
+        {
+            { "win10", new WindowsVersion(
+                productName: "Microsoft Windows 10",
+                csdVersion: "",
+                currentBuild: "17763",
+                currentBuildNumber: "17763",
+                currentVersion: "10.0"
+            ) },
+            { "win81", new WindowsVersion(
+                productName: "Microsoft Windows 8.1",
+                csdVersion: "",
+                currentBuild: "9600",
+                currentBuildNumber: "9600",
+                currentVersion: "6.3"
+            ) },
+            { "win8", new WindowsVersion(
+                productName: "Microsoft Windows 8",
+                csdVersion: "",
+                currentBuild: "9200",
+                currentBuildNumber: "9200",
+                currentVersion: "6.2"
+            ) },
+            { "win7", new WindowsVersion(
+                productName: "Microsoft Windows 7",
+                csdVersion: "Service Pack 1",
+                currentBuild: "7601",
+                currentBuildNumber: "7601",
+                currentVersion: "6.1"
+            ) },
+            { "win2008r2", new WindowsVersion(
+                productName: "Microsoft Windows 2008 R2",
+                csdVersion: "Service Pack 1",
+                currentBuild: "7601",
+                currentBuildNumber: "7601",
+                currentVersion: "6.1"
+            ) },
+            { "win2008", new WindowsVersion(
+                productName: "Microsoft Windows 2008",
+                csdVersion: "Service Pack 2",
+                currentBuild: "6002",
+                currentBuildNumber: "6002",
+                currentVersion: "6.0"
+            ) },
+            { "winxp", new WindowsVersion(
+                productName: "Microsoft Windows XP",
+                csdVersion: "Service Pack 2",
+                currentBuild: "3790",
+                currentBuildNumber: "3790",
+                currentVersion: "5.2"
+            ) },
+        };
+
+
+        public Wine(string winePath, string winePrefixPath, int verboseLevel = 0)
         {
             this.WinePath = winePath;
             this.WinePrefixPath = winePrefixPath;
+            this.VerboseLevel = 3;
 
             if (Enum.IsDefined(typeof(VerboseLevels), verboseLevel))
                 this.VerboseLevel = verboseLevel;
-            else
-                this.VerboseLevel = 3;     
 
-            
             if (!ValidateWinePath())
                 throw new Exception("Wine Path is not valid");
         }
+
         private bool ValidateWinePath()
         {
             if (string.IsNullOrEmpty(this.WinePath))
