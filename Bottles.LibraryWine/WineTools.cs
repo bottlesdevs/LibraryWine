14755567edb9be49b26ed20faf8cb6cab740bf6e
mirkobrombin <send@mirko.pm>
2021-07-17 23:45:52 +0200

    WineCfg,WineDbg

--- Bottles.LibraryWine/WineTools.cs
+++ Bottles.LibraryWine/WineTools.cs
@@ -7,10 +7,20 @@ namespace Bottles.LibraryWine
         public static string GetVersion(ref Wine wine)
         {
             var result = wine.ExecCommand("--version", getOutput: true);
-            if(result is bool)
+            if (result is bool)
                 return result.ToString();
 
             return (string)result;
+        }
+
+        public static void WineCfg(ref Wine wine)
+        {
+            wine.ExecCommand("winecfg");
+        }
+
+        public static void WineDbg(ref Wine wine)
+        {
+            wine.ExecCommand("winedbg", useTerminal: true);
         }
     }
 }
